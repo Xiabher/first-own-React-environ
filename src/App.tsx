ed1c034ba03fb8b264932649645da53d99b4033b
Xiabher <99346955+Xiabher@users.noreply.github.com>
2023-06-17 19:52:35 +0200

    I made it ! In stackblitz ! I made my truly first app in react. From this point on it is forward.

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function Header() {
       <header>
           <nav className = "nav">
           <ul className="nav-items">
-            
+            <img src = "./react-logo.png" className ="nav-logo"/>
                     <li>Pricing</li>
                     <li>About</li>
                     <li>Contact</li>
