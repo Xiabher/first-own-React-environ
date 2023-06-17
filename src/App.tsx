165d52174d31bfc3c4d572c827ca8e62564759f7
Xiabher <99346955+Xiabher@users.noreply.github.com>
2023-06-17 19:46:36 +0200

    1 binaries uploaded

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,53 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
+}
+
+function MainContent() {
+  return (
+      <div>
+          <h1>Reasons I'm excited to learn React</h1>
+         
+          <ol>
+              <li>It's a popular library, so I'll be 
+              able to fit in with the cool kids!</li>
+              <li>I'm more likely to get a job as a developer
+              if I know React</li>
+              <h3>Hello StackBlitz!</h3>
+              <p>Start editing to see some magic happen :)</p>
+          </ol>
+      </div>
+  )
+}
+
+function Header() {
+  return (
+      <header>
+          <nav className = "nav">
+          <ul className="nav-items">
+            
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+             
+              
+          </nav>
+      </header>
+  )
+}
+
+
+
+function Footer() {
+  return (
+      <footer>
+          <small>© 2023 Xiabher develops. All rights reserved.</small>
+      </footer>
+  )
 }
