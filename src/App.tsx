5b1c5006ab482fbfa7d0c840dbdc582c43f01adf
Xiabher <99346955+Xiabher@users.noreply.github.com>
2023-06-19 11:33:08 +0200

    MainContent in a separate file 1 file modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './style.css';
 import Header from "./Header"
+import MainContent from './MainContent'
 
 export default function App() {
   return (
@@ -12,22 +13,7 @@ export default function App() {
   );
 }
 
-function MainContent() {
-  return (
-      <div>
-          <h1>Reasons I'm excited to learn React</h1>
-         
-          <ol>
-              <li>It's a popular library, so I'll be 
-              able to fit in with the cool kids!</li>
-              <li>I'm more likely to get a job as a developer
-              if I know React</li>
-              <h3>Hello StackBlitz!</h3>
-              <p>Start editing to see some magic happen :)</p>
-          </ol>
-      </div>
-  )
-}
+
 
 
 
---
+++ src/MainContent.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+export default function MainContent() {
+  return (
+      <div>
+          <h1>Reasons I'm excited to learn React</h1>
+         
+          <ol>
+              <li>It's a popular library, so I'll be 
+              able to fit in with the cool kids!</li>
+              <li>I'm more likely to get a job as a developer
+              if I know React</li>
+              <h3>Hello StackBlitz!</h3>
+              <p>Start editing to see some magic happen :)</p>
+          </ol>
+      </div>
+  )
+}
